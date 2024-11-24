40d57ebb8b071abc33d821b030d00d9a738c59c2
amba404 <amba.avg@gmail.com>
2024-11-24 22:23:07 +0900

    Initial

---
+++ src/test/java/org/skyjava/course2/Course2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skyjava.course2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
